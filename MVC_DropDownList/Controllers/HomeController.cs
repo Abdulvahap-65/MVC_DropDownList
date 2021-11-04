f52d59797b5becb418024e2d8263557293e32e43
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2021-11-04 10:53:24 +0300

    Add project files.

---
+++ MVC_DropDownList/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_DropDownList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            int kategoriID = 2;
+            List<SelectListItem> kategoriler = new List<SelectListItem>();
+            foreach (var item in db.Categories.ToList())
+            { 
+                kategoriler.Add(
+                    new SelectListItem {
+                        Text = item.CategoryName,
+                        Value = item.CategoryID.ToString(),                    
+                        Selected = (item.CategoryID == kategoriID ? true : false),
+                    });
+            }
+            ViewBag.Kategoriler = kategoriler;
+            return View();
+        }
+        public ActionResult Calisma()
+        {
+
+
+            return View();
+        }
+    }
+}
